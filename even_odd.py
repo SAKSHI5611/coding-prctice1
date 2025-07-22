0160d009f2a134b122e30966d1d7be841ebc0b2b
SAKSHI5611 <sakshijadhav5611@gmail.com>
2025-07-22 20:59:27 +0530

    Add files via upload
    python program even or odd

---
+++ even_odd.py
@@ -0,0 +1,11 @@
+def evenodd( number):
+    if number%2==0:
+        print("num is even")
+    else:
+        print("number is odd")
+
+def main():
+    num=int(input("enter the number:"))
+    evenodd(num)
+
+main()
